3da061d6f0751abc0e8994ac9e74aa0b62a96431
Anna Taranova <annsheremet123@gmail.com>
2025-05-11 21:01:14 +0200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,23 @@
-const API_KEY = '50174992-a37a037319a3aab3105cbedec';  
+import axios from 'axios';
+
+const API_KEY = '50174992-a37a037319a3aab3105cbedec';
 const BASE_URL = 'https://pixabay.com/api/';
 
-
 export async function fetchImages(query) {
-  const params = new URLSearchParams({
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
     per_page: 9,
-  });
+  };
 
   try {
-    const response = await fetch(`${BASE_URL}?${params}`);
-    if (!response.ok) {
-      throw new Error('Error fetching images');
-    }
-    return await response.json();  
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
   } catch (error) {
-    console.error(error);
-    throw error;  
+    console.error('Error fetching images:', error);
+    throw error;
   }
 }
-
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -78,12 +78,14 @@ export function clearGallery() {
   }
 }
 
-
 export function showLoader() {
+  const loader = document.querySelector('#loader');
   if (loader) loader.classList.remove('hidden');
 }
 
-
 export function hideLoader() {
+  const loader = document.querySelector('#loader');
   if (loader) loader.classList.add('hidden');
 }
+
+
